34dd488c084d16e9ca1b5014967d485a274e894f
Shohei Fujii <fujii.shohei@gmail.com>
2018-08-21 18:47:49 +0900

    initial commit

---
+++ api.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+class my_plugin_api {
+public:
+   virtual std::string name() const = 0;
+   virtual float calculate(float x, float y) = 0;
+
+   virtual ~my_plugin_api() {}
+};
